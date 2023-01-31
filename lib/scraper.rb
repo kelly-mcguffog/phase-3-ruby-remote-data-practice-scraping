412cb6e9caf66052c151929009f9b5b3da9a43db
Kelly McGuffog <kellymcguffog@gmail.com>
2023-01-31 15:11:43 -0500

    commit tree 9def549b6a55b612110f46f3b1a9f3ac0c4e8626

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+# doc.css(".headline-26OIBN").text
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
